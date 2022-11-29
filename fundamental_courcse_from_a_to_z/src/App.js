c4a4bd7128d02fe75e43b136fc1845b725c4ed7a
GeorgeSemenov <zhora.sem@yandex.ru>
2022-11-29 12:16:11 +0300

    axios complete

--- fundamental_courcse_from_a_to_z/src/App.js
+++ fundamental_courcse_from_a_to_z/src/App.js
@@ -8,14 +8,15 @@ import PostFilter from './components/PostFilter.jsx'
 import MyButton from './components/UI/button/MyButton.jsx'
 import MyModal from './components/UI/MyModal/MyModal.jsx'
 import {usePosts} from './hooks/usePosts.js';
+import axios from 'axios';
 
 import './styles/App.css';
 
 function App() {
   const [posts,setPosts] = useState([
-    {id:1, title: "JS-post-title", body: "JavaScript - is programm language"},
-    {id:2, title: "php-post-title", body: "python - is programm language"},
-    {id:3, title: "python-post-title", body: "php - is programm language"},
+    // {id:1, title: "JS-post-title", body: "JavaScript - is programm language"},
+    // {id:2, title: "php-post-title", body: "python - is programm language"},
+    // {id:3, title: "python-post-title", body: "php - is programm language"},
   ]) 
   const [modal,setModal] = useState(false)
   const [filter,setFilter] = useState({sort:'',query:''}) 
@@ -26,12 +27,19 @@ function App() {
     setPosts([...posts, newPost])
     setModal(false);
   }
+
+  async function fetchPosts(){//Функция асинхронная, чтобы можно было использовать await
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    setPosts(response.data);
+  }
+
   const removePostFromPosts = (post)=>{
     setPosts(posts.filter((item)=>{return item.id !==post.id}))
   }
 
   return (
     <div className="App">
+      <button onClick = {fetchPosts}>GET POSTS</button>
       <MyButton
         style={{marginTop: "30px"}}
         onClick = {()=>setModal(true)}
