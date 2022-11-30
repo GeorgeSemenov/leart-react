0a9f5975e2c2db1db713cae1c5181d1bee2b5589
GeorgeSemenov <zhora.sem@yandex.ru>
2022-11-30 05:50:39 +0300

    life cicle of component

--- fundamental_courcse_from_a_to_z/src/App.js
+++ fundamental_courcse_from_a_to_z/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState,useRef,useMemo} from 'react';
+import React, {useState,useRef,useMemo, useEffect} from 'react';
 import Counter from './components/Counter.jsx';
 import ClassCounter from './components/ClassCounter.jsx';
 import PostList from './components/PostList.jsx';
