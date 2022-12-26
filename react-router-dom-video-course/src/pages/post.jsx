a3015f0ba89cb717d26fc2d88bd1a05e817dcef8
GeorgeSemenov <zhora.sem@yandex.ru>
2022-12-26 08:17:36 +0300

    try to add defer to post.jsx

--- react-router-dom-video-course/src/pages/post.jsx
+++ react-router-dom-video-course/src/pages/post.jsx
@@ -5,7 +5,7 @@ import {
 } from "react-router-dom";
 
 function Post() {
-  const id = useLoaderData();
+  const {id,post} = useLoaderData();
   const navigate = useNavigate(); //Хук возвращает функцию ,которую мы и будем использовать.
   const goBack = () => navigate("/blockPage",{state: "it's my state, baby"}); //Создаём новую функцию ,которая будет вызывать navigate со специальными атрибутами. Можно указать напрямую относительный путь, но это мало чем отличается от Link, но если указывать отрицательные цифры, (-1, -2 ...) то он вернётся ну указанное количество страниц назад. Если же нужно указать движение вперёд, то нужно указывать положительные цифири
 
@@ -16,8 +16,10 @@ function Post() {
       >
         Go v zad
       </button>
-      Привет, это пост.
+      Привет, это пост 
+      <h1>{post.title}</h1>
       id данного поста = {id}
+      <p>{post.body}</p>
       <Link to={`edit`}>
         Редактировать.
       </Link>
@@ -27,8 +29,10 @@ function Post() {
 
 export default Post;
 
-const postLoader = ({params})=>{
+const postLoader = async ({params})=>{
   const id = params.id;//Поле id - это имя динамической переменной, указанной в таблице маршрутизации
-  return id;
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+  const post = await res.json();
+  return {id,post};
 }
 export {postLoader};
