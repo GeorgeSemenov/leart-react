b2cced68498ab4beecc999a361f117ffaa871588
GeorgeSemenov <zhora.sem@yandex.ru>
2023-01-21 06:18:22 +0300

    add navbar

--- fundamental_courcse_from_a_to_z/src/App.js
+++ fundamental_courcse_from_a_to_z/src/App.js
@@ -1,13 +1,26 @@
 import React, {useState,useRef,useMemo, useEffect} from 'react';
 import './styles/App.css';
-import {BrowserRouter,Route} from 'react-router-dom';
+import {
+  RouterProvider,
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom';
+import Navbar from './components/Navbar.jsx';
 import About from './pages/About.jsx';
+import Posts from "./pages/Posts.jsx";
+
+const router=createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Navbar/>}>
+      <Route path="/about" element = {<About/>}/>
+    </Route>
+  )
+);
 
 function App() {
   return(
-    <BrowserRouter>
-      <Route exact path="/about" element = {<About/>}/>
-    </BrowserRouter>
+    <RouterProvider router={router}/>
   )
 }
 
