64ac778d1c50fd47307d8286d76ffd543a0d4d3f
GeorgeSemenov <zhora.sem@yandex.ru>
2023-01-24 08:22:39 +0300

    AuthContext created

--- fundamental_courcse_from_a_to_z/src/App.js
+++ fundamental_courcse_from_a_to_z/src/App.js
@@ -2,11 +2,14 @@ import React, {useState,useRef,useMemo, useEffect} from 'react';
 import './styles/App.css';
 import {RouterProvider} from 'react-router-dom';
 import {publicRouter, privateRouter} from './router/index.js';
+import {AuthContext} from './context';
 
 function App() {
   let isAuth = false;
   return(
-    <RouterProvider router={isAuth? privateRouter: publicRouter}/>
+    <AuthContext.Provider>
+      <RouterProvider router={isAuth? privateRouter: publicRouter}/>
+    </AuthContext.Provider>
   )
 }
 
